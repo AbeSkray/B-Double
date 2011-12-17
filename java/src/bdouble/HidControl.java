2585a255ead4bff02688fe08fac17125763b42cd
Alexander Knowles <abe.skray@gmail.com>
2011-12-16 22:07:53 -0500

    new class to represent gamepad input controls; using as a test main for now

---
+++ java/src/bdouble/HidControl.java
@@ -0,0 +1,35 @@
+/**
+ * Object to handle user input from a Gamepad or other HID
+ * 
+ * @author aknowles
+ */
+package bdouble;
+
+import java.io.IOException;
+
+import com.codeminders.hidapi.*;
+
+public class HidControl {
+
+	/**
+	 * Test Main
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String property = System.getProperty("java.library.path");
+		System.out.println(property);
+		
+		HIDDeviceInfo[] deviceInfos;
+		try {
+			deviceInfos = HIDManager.listDevices();
+			
+			// print each device info
+			for (int i = 0; i < deviceInfos.length; ++i) {
+				System.out.println(deviceInfos[i]);
+			}
+		} catch (IOException ioe) {
+			System.err.println("Exception thrown");
+			ioe.printStackTrace();
+		}
+	}
+}
