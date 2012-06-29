c83f38223c0157d22385e30df906d5dae9008eb2
Alexander Knowles <abe.skray@gmail.com>
2012-06-28 22:10:17 -0500

    Map left and right key inputs to steering.

--- java/src/bdouble/KeyboardController.java
+++ java/src/bdouble/KeyboardController.java
@@ -1,6 +1,9 @@
 package bdouble;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import bdouble.Controller.Steering;
 
 /**
  * Controller that receives input from keyboard events
@@ -9,21 +12,29 @@ public class KeyboardController
     extends KeyAdapter
     implements Controller
 {
-    private Steering steering;
+
     private Transmission transmission;
+
+    private boolean leftKeyIsDown = false;
+    private boolean rightKeyIsDown = false;
 
     /**
      * Constructor
      */
     public KeyboardController() {
         super();
-        this.steering = Steering.CENTER;
         this.transmission = Transmission.NEUTRAL;
     }
 
     @Override
     public Steering getSteeringInput() {
-        return this.steering;
+        Steering result = Steering.CENTER;
+        if (leftKeyIsDown && !rightKeyIsDown) {
+            result = Steering.LEFT_1;
+        } else if (rightKeyIsDown && !leftKeyIsDown) {
+            result = Steering.RIGHT_1;
+        }
+        return result;
     }
 
     @Override
@@ -31,4 +42,35 @@ public class KeyboardController
         return this.transmission;
     }
 
+    @Override
+    public void keyPressed(KeyEvent e) {
+        final int keyCode = e.getKeyCode();
+
+        if ( (keyCode & KeyEvent.KEY_LOCATION_LEFT) 
+                == KeyEvent.KEY_LOCATION_LEFT)
+        {
+            this.leftKeyIsDown = true;
+        }
+        if ( (keyCode & KeyEvent.KEY_LOCATION_RIGHT)
+                == KeyEvent.KEY_LOCATION_RIGHT)
+        {
+            this.rightKeyIsDown = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        final int keyCode = e.getKeyCode();
+
+        if ( (keyCode & KeyEvent.KEY_LOCATION_LEFT) 
+                == KeyEvent.KEY_LOCATION_LEFT)
+        {
+            this.leftKeyIsDown = false;
+        }
+        if ( (keyCode & KeyEvent.KEY_LOCATION_RIGHT)
+                == KeyEvent.KEY_LOCATION_RIGHT)
+        {
+            this.rightKeyIsDown = false;
+        }
+    }
 }
